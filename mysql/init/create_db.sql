454925dbb6f47381629c38ef5ee762bae1907152
trasta <trasta298@gmail.com>
2022-06-14 23:48:33 +0900

    ✨ mariaDBの追加

---
+++ mysql/init/create_db.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS mushikui;
+CREATE DATABASE mushikui;
+USE mushikui;
+
+CREATE TABLE IF NOT EXISTS `problems` (
+    `id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `expression` varchar(32) NOT NULL,
+    `date` datetime NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
